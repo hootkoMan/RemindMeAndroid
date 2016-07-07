02053078f96333301711310d379bf2ffd77a8f29
ruslan <ruslan.bur@gmail.com>
2016-07-07 14:27:10 +0300

    Implemented  Tabs, test fragment

---
+++ app/src/main/java/com/company/irus/remindmetwo/Constants.java
@@ -0,0 +1,10 @@
+package com.company.irus.remindmetwo;
+
+/**
+ * Created by iRus on 7/7/2016.
+ */
+public class Constants {
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+}
--- app/src/main/java/com/company/irus/remindmetwo/MainActivity.java
+++ app/src/main/java/com/company/irus/remindmetwo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.company.irus.remindmetwo;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -55,5 +57,35 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.view_navigation_open,
+                R.string.view_navigation_close);
+
+//        drawerLayout.setDrawerListener(toggle); //deprecated
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.actionNotificationItem:
+                        showNotificationTab();
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+    private void showNotificationTab() {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }

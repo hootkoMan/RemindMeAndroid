ecaaee37e4f09be26c0da92e0196d8ebc11a57eb
ruslan <ruslan.bur@gmail.com>
2016-07-10 01:10:31 +0300

    Added CardView

---
+++ app/src/main/java/com/company/irus/remindmetwo/dto/RemindDTO.java
@@ -0,0 +1,21 @@
+package com.company.irus.remindmetwo.dto;
+
+/**
+ * Created by iRus on 7/9/2016.
+ */
+public class RemindDTO {
+
+    private String title;
+
+    public RemindDTO(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- app/src/main/java/com/company/irus/remindmetwo/fragment/HistoryFragment.java
+++ app/src/main/java/com/company/irus/remindmetwo/fragment/HistoryFragment.java
@@ -3,18 +3,25 @@ package com.company.irus.remindmetwo.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.company.irus.remindmetwo.R;
+import com.company.irus.remindmetwo.adapter.RemindListAdapter;
+import com.company.irus.remindmetwo.dto.RemindDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by iRus on 7/7/2016.
  */
 public class HistoryFragment extends AbstractTabFragment {
 
-    private static final int LAYOUT = R.layout.fragment_example;
+    private static final int LAYOUT = R.layout.fragment_history;
 
     public static HistoryFragment getInstance(Context context) {
         Bundle args = new Bundle();
@@ -30,7 +37,23 @@ public class HistoryFragment extends AbstractTabFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.recycleView);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new RemindListAdapter(createMockRemindListData()));
+
         return view;
+    }
+
+    private List<RemindDTO> createMockRemindListData() {
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("item 1"));
+        data.add(new RemindDTO("item 2"));
+        data.add(new RemindDTO("item 3"));
+        data.add(new RemindDTO("item 4"));
+        data.add(new RemindDTO("item 5"));
+        data.add(new RemindDTO("item 6"));
+        return data;
     }
 
     public void setContext(Context context) {
